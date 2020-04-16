94093910b9972a232cc77c2059faf9a2ba4841cb
Arnoud de Boer <a.deboer@geonovum.nl>
2020-04-16 11:02:54 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,22 @@
 var respecConfig = {
   specStatus: "GN-DEF",
   specType: "IM",
-  publishDate: "2020-03-31",  
+  subtitle: "BRO gegevenscatalogus",
+  publishDate: "2020-03-31", 
+  logos: [{
+    src: 'logo_minbzk',
+    href: "http://basisregistratieondergrond.nl",
+    alt: "Ministerie BZK",
+    width: 130,
+    height: 250,
+    id: 'BZK',
+  }]  ,
   editors: [],
   authors: [
     {
       name: "TNO Geologische Dienst Nederland",
-	  email: "Support@broservicedesk.nl",
-	  telephone: "088 – 8664 999",
+	  mailto: "Support@broservicedesk.nl",
+	  note: "Contact 	Support@broservicedesk.nl / 088 – 8664 999",
       companyURL: "http://www.tno.nl",
     }],
   shortName: "bro-GTM",
@@ -15,13 +24,13 @@ var respecConfig = {
   github: "https://github.com/BROprogramma/GTM",
   //previousPublishDate: "2017-01-01",
   //previousStatus: "GN-CV",
-  localBiblio: {
-    "ISO19136": {
-      "href": "https://www.iso.org/standard/32554.html",
-      "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
-      "authors": [""],
-      "date": "September 2007",
-      "publisher": "International Organization for Standardization",
-    },
-  },
+ // localBiblio: {
+ //   "ISO19136": {
+  //    "href": "https://www.iso.org/standard/32554.html",
+  //    "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
+  //    "authors": [""],
+ //     "date": "September 2007",
+ //     "publisher": "International Organization for Standardization",
+ //   },
+  //},
 };
