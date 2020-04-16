b9ecbcc8236a17239a3191c8212f2be98113a990
Arnoud de Boer <a.deboer@geonovum.nl>
2020-04-16 09:41:33 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,25 +1,9 @@
 var respecConfig = {
-  specStatus: "GN-WV",
+  specStatus: "GN-DEF",
   specType: "IM",
-  //publishDate: "2018-10-15",
-  editors: [{
-    name: "Arnoud de Boer",
-    company: "Geonovum",
-    companyURL: "https://www.geonovum.nl"
-  }],
-   authors: [
+  publishDate: "2020-04-01",  
+  authors: [
     {
-      name: "Jan Hummelman",
-      company: "TNO",
-      companyURL: "http://www.tno.nl",
-    },
-	{
-      name: "Jan Stafleu",
-      company: "TNO",
-      companyURL: "http://www.tno.nl",
-    },
-	{
-      name: "Rob van Ede",
       company: "TNO",
       companyURL: "http://www.tno.nl",
     }],
@@ -28,13 +12,13 @@ var respecConfig = {
   github: "https://github.com/BROprogramma/GTM",
   //previousPublishDate: "2017-01-01",
   //previousStatus: "GN-CV",
-  //localBiblio: {
-  //  "ISO19136": {
-  //    "href": "https://www.iso.org/standard/32554.html",
-  //    "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
-  //    "authors": [""],
-  //    "date": "September 2007",
-  //    "publisher": "International Organization for Standardization",
-  //  },
-  //},
+  localBiblio: {
+    "ISO19136": {
+      "href": "https://www.iso.org/standard/32554.html",
+      "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
+      "authors": [""],
+      "date": "September 2007",
+      "publisher": "International Organization for Standardization",
+    },
+  },
 };
